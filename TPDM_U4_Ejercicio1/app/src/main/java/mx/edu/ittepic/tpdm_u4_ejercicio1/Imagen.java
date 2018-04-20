1d825ee97dad6bc237a458277a5acc44ed91e4d3
Brian <bralcasaslo@ittepic.edu.mx>
2018-04-20 08:39:45 -0600

    Clase de 19/04/2018 Se agregó la clase Imagen

--- TPDM_U4_Ejercicio1/app/src/main/java/mx/edu/ittepic/tpdm_u4_ejercicio1/Imagen.java
+++ TPDM_U4_Ejercicio1/app/src/main/java/mx/edu/ittepic/tpdm_u4_ejercicio1/Imagen.java
@@ -13,6 +13,24 @@ public class Imagen {
         y = _y;
     }//Fin constructor
 
-    
+    public boolean estaEnArea(int xp, int yp){ //PUNTEROS xp y yp
+        int x2 = x+img.getWidth();
+        int y2 = y+img.getHeight();
+
+        if(xp >= x && xp <= x2){
+            if(yp >= y && yp <= y2){
+                return true;
+            }
+        }
+        return true;
+    }
 
 }//Fin clase
+
+/*if(e.getX() >= posXimagen && e.getX() <= posXimagen+imagen.getWidth()){
+                    if(e.getY() >= posYimagen && e.getY() <= posYimagen+imagen.getHeight()){
+                        posXimagen = (int) e.getX() - imagen.getWidth()/2;
+                        posYimagen = (int) e.getY()- imagen.getHeight()/2;
+                        return true;
+                    }
+                }//fin if*/
--- TPDM_U4_Ejercicio1/app/src/main/java/mx/edu/ittepic/tpdm_u4_ejercicio1/Lienzo.java
+++ TPDM_U4_Ejercicio1/app/src/main/java/mx/edu/ittepic/tpdm_u4_ejercicio1/Lienzo.java
@@ -14,9 +14,8 @@ public class Lienzo extends View { //View necesita saber quien lo va a invocar
 
     CountDownTimer t;
     int desplazamientoX = 5;
-    Bitmap imagen, imagen2, imagen3, imagen4;
-    int posXimagen, posYimagen;
-    int posXimagen2, posYimagen2;
+    Imagen imagen, imagen2;
+
 
     public Lienzo(Context context) {
         super(context);
@@ -35,14 +34,9 @@ public class Lienzo extends View { //View necesita saber quien lo va a invocar
         };
 
         t.start();
-        imagen = BitmapFactory.decodeResource(getResources(), R.drawable.link);
-        imagen2 = BitmapFactory.decodeResource(getResources(), R.drawable.cascomc);
+        imagen = new Imagen(BitmapFactory.decodeResource(getResources(), R.drawable.link));
+        imagen2 = new Imagen(BitmapFactory.decodeResource(getResources(), R.drawable.cascomc));
 
-        posXimagen = 50;
-        posYimagen = 500;
-
-        posXimagen = 100;
-        posYimagen = 250;
     }//Fin constructor
 
     protected void onDraw(Canvas c){
@@ -67,20 +61,6 @@ public class Lienzo extends View { //View necesita saber quien lo va a invocar
                 break;
             case MotionEvent.ACTION_MOVE:
                 //Programa el estado "ARRASTRE"
-                if(e.getX() >= posXimagen && e.getX() <= posXimagen+imagen.getWidth()){
-                    if(e.getY() >= posYimagen && e.getY() <= posYimagen+imagen.getHeight()){
-                        posXimagen = (int) e.getX() - imagen.getWidth()/2;
-                        posYimagen = (int) e.getY()- imagen.getHeight()/2;
-                        return true;
-                    }
-                }//fin if
-
-                if(e.getX() >= posXimagen2 && e.getX() <= posXimagen2+imagen2.getWidth()){
-                    if(e.getY() >= posYimagen2 && e.getY() <= posYimagen2+imagen2.getHeight()){
-                        posXimagen2 = (int) e.getX() - imagen2.getWidth()/2;
-                        posYimagen2 = (int) e.getY()- imagen2.getHeight()/2;
-                    }
-                }//fin if
                 break;
             case MotionEvent.ACTION_UP:
                 //Programa el estado "SOLTAR"
